6811b733591b7001fa5993851c78e9df3d55e36c
coperand <coperand199@yandex.ru>
2025-04-19 16:39:05 +0300

    Начальный коммит

---
+++ a2/a2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(
+            "     *\n"
+            "    ***\n"
+            "   *****\n"
+            "  *******\n"
+            "   HHOHH\n"
+            "   ZZZZZ\n"
+          );
+}
